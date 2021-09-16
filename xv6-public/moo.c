dc7c364c0cb949b7e08501888dd7999e98935598
Lucía Martinez Gavier <lucia.m.gavier@mi.unc.edu.ar>
2021-09-16 16:39:34 -0300

    agregada xv6-public

---
+++ xv6-public/moo.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  printf(1, "                 (__)\n");
+  printf(1, "                 (oo)\n");
+  printf(1, "           /------\\/\n");
+  printf(1, "          / |    ||\n");
+  printf(1, "         *  /\\---/\\\n");
+  printf(1, "            ~~   ~~\n");
+  exit();
+}
