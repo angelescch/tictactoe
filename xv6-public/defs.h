d18cefded70c62657ef79be4b286a5c994e104a4
Bruno-C137 <castebruno@gmail.com>
2021-10-01 19:35:20 -0300

    habemus fuentes (desprolijo pero funcional :D)

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -22,6 +22,7 @@ void            cprintf(char*, ...);
 void            consoleintr(int(*)(void));
 void            panic(char*) __attribute__((noreturn));
 int             getc(void);
+
 // vga.c
 void            vgainit(void);
 void            modeswitch(int);
